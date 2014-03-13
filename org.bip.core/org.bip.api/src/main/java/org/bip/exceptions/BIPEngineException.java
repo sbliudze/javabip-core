a56e80b7a3605ee7df758578d6b7fe209a2f65f6
Radoslaw Szymanek <radoslaw.szymanek@crossing-tech.com>
2014-03-13 17:02:27 +0100

    Adding serialUID

--- org.bip.core/org.bip.api/src/main/java/org/bip/exceptions/BIPEngineException.java
+++ org.bip.core/org.bip.api/src/main/java/org/bip/exceptions/BIPEngineException.java
@@ -2,6 +2,8 @@ package org.bip.exceptions;
 
 public class BIPEngineException extends RuntimeException {
 
+	private static final long serialVersionUID = 1L;
+
 	public BIPEngineException(String message)
 	{
 		super(message);
--- org.bip.core/org.bip.api/src/main/java/org/bip/exceptions/BIPException.java
+++ org.bip.core/org.bip.api/src/main/java/org/bip/exceptions/BIPException.java
@@ -2,6 +2,8 @@ package org.bip.exceptions;
 
 public class BIPException extends RuntimeException {
 
+	private static final long serialVersionUID = 1L;
+
 	public BIPException(String message)
 	{
 		super(message);
