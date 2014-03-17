76332373716b2b9fc46f4631cc121b056aef1d0b
Anastasia Mavridou <anastasia.mavridou@epfl.ch>
2014-03-17 10:56:53 +0100

    Correction in MultipleSpontaneous test

--- org.bip.core/org.bip.spec.examples/src/main/java/org/bip/spec/PSSComponent.java
+++ org.bip.core/org.bip.spec.examples/src/main/java/org/bip/spec/PSSComponent.java
@@ -17,54 +17,55 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @bipPorts({ @bipPort(name = "s1", type = "spontaneous"),
-            @bipPort(name = "s2", type = "spontaneous"),
-            @bipPort(name = "p", type = "enforceable") })
-@bipComponentType(initial = "start", name = "org.bip.spec.PComponent")
+		@bipPort(name = "s2", type = "spontaneous"),
+		@bipPort(name = "p", type = "enforceable") })
+@bipComponentType(initial = "start", name = "org.bip.spec.PSSComponent")
 public class PSSComponent {
 
-    Logger logger = LoggerFactory.getLogger(SwitchableRoute.class);
+	Logger logger = LoggerFactory.getLogger(SwitchableRoute.class);
 
-    public int pCounter = 0;
+	public int pCounter = 0;
 
-    int pEnabled = 0;
+	int pEnabled = 0;
 
-    boolean needExternalEnable;
+	boolean needExternalEnable;
 
-    public int spontaneousEnableCounter = 0;
+	public int spontaneousEnableCounter = 0;
 
-    public int spontaneousDisableCounter = 0;
+	public int spontaneousDisableCounter = 0;
 
-    public PSSComponent(boolean needExternalEnable) {
+	public PSSComponent(boolean needExternalEnable) {
 
-        this.needExternalEnable = needExternalEnable;
-    }
+		this.needExternalEnable = needExternalEnable;
+	}
 
-    /*
-      * Check what are the conditions for throwing the exception.
-      */
-    @bipTransition(name = "p", source = "start", target = "start", guard = "isPEnabled")
-    public void enforceableP() throws Exception {
-        logger.debug("P transition is being executed.");
-        pCounter++;
-        pEnabled --;
-    }
+	/*
+	 * Check what are the conditions for throwing the exception.
+	 */
+	@bipTransition(name = "p", source = "start", target = "start", guard = "isPEnabled")
+	public void enforceableP() throws Exception {
+		logger.debug("P transition is being executed.");
+		pCounter++;
+		pEnabled--;
+	}
 
-    @bipGuard(name = "isPEnabled")
-    public boolean isPEnabled() {
-        return !needExternalEnable || pEnabled > 0;
-    }
+	@bipGuard(name = "isPEnabled")
+	public boolean isPEnabled() {
+		return !needExternalEnable || pEnabled > 0;
+	}
 
-    @bipTransition(name = "s1", source = "start", target = "start")
-    public void enableP() {
-    	logger.debug("s1 transition is being executed.");
-        pEnabled++;;
-        spontaneousEnableCounter++;
-    }
+	@bipTransition(name = "s1", source = "start", target = "start")
+	public void enableP() {
+		logger.debug("s1 transition is being executed.");
+		pEnabled++;
+		;
+		spontaneousEnableCounter++;
+	}
 
-    @bipTransition(name = "s2", source = "start", target = "start")
-    public void disableP() {
-    	logger.debug("s2 transition is being executed.");
-        pEnabled --;
-        spontaneousDisableCounter++;
-    }
+	@bipTransition(name = "s2", source = "start", target = "start")
+	public void disableP() {
+		logger.debug("s2 transition is being executed.");
+		pEnabled--;
+		spontaneousDisableCounter++;
+	}
 }
