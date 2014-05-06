466b77364d16b7e338a0441e82a74dff6b1bedd4
Radoslaw Szymanek <radoslaw.szymanek@crossing-tech.com>
2014-05-06 14:54:05 +0200

    Removing unncessary enum from PortType

--- org.bip.core/org.bip.api/src/main/java/org/bip/api/PortType.java
+++ org.bip.core/org.bip.api/src/main/java/org/bip/api/PortType.java
@@ -7,8 +7,6 @@ public enum PortType {
 		/** This port is associated with spontaneous transition. */
 		spontaneous,
 		/** This port is associated with internal transition that does not require any external trigger. */
-		internal,
-		/** This type is simply to make it possible to lazily instantiate the actual type of the port. */
-		unknown	
+		internal
 
 }
