f952081aaf92506a99fbbe2de56ac0186458d007
Radoslaw Szymanek <radoslaw.szymanek@crossing-tech.com>
2014-05-30 11:21:14 +0200

    Refactoring BIPEngine bundle for usecase for new actor based BIPEngine

--- bip.usecase.cf/bipengine/src/main/java/io/xtech/bip/activator/BIPEngineActivator.java
+++ bip.usecase.cf/bipengine/src/main/java/io/xtech/bip/activator/BIPEngineActivator.java
@@ -6,79 +6,41 @@ package io.xtech.bip.activator;
  * Copyright 2012 Crossing-Tech
  */
 
-import java.util.Map;
-import java.util.Set;
-
-import org.bip.api.BIPComponent;
 import org.bip.api.BIPEngine;
-import org.bip.api.BIPGlue;
-import org.bip.api.Behaviour;
-import org.bip.api.Port;
 import org.bip.engine.BIPCoordinatorImpl;
 import org.bip.engine.DataCoordinatorImpl;
+import org.bip.engine.api.EngineFactory;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+
+import akka.actor.ActorSystem;
 
 public class BIPEngineActivator implements BundleActivator {
 
 	volatile BIPEngine engine;
 
 	public void start(BundleContext ctx) throws Exception {
-		engine = new DataCoordinatorImpl(new BIPCoordinatorImpl());
-
-		try {
-			ctx.registerService(BIPEngine.class.getName(), new BIPEngine() {
-				public void specifyGlue(BIPGlue glue) {
-					System.out.println("Glue specification has been provided ");
-					engine.specifyGlue(glue);
-				}
-
-				public void register(BIPComponent component, Behaviour behaviour) {
-					engine.register(component, behaviour);
-				}
-
-				public void start() {
-					engine.start();
-					System.out.println("BIP Engine was told to start");
-				}
-
-				public void stop() {
-					engine.stop();
-					System.out.println("BIP Engine was told to stop");
-				}
-
-				public void execute() {
-					engine.execute();
-					System.out.println("BIP Engine was told to execute");
-
-				}
-
-
-				public void inform(BIPComponent component, String currentState,
-						Set<Port> disabledPorts) {
-					engine.inform(component, currentState, disabledPorts);
-					
-				}
-
-				public void informSpecific(BIPComponent decidingComponent,
-						Port decidingPort,
-						Map<BIPComponent, Set<Port>> disabledCombinations) {
-					engine.informSpecific(decidingComponent, decidingPort, disabledCombinations);
-					
-				}
-			}, null);
-		} catch (Exception ex) {
-			System.out.println(ex);
-			throw ex;
-		}
-
-		engine.start();
-		System.out.println("Bundle providing BIP Engine for Simple usecase is started.");
+		
+//		ActorSystem system = ActorSystem.create("MySystem");
+//		EngineFactory engineFactory = new EngineFactory(system);
+//		BIPEngine engine = engineFactory.create("myEngine",
+//				new DataCoordinatorImpl(new BIPCoordinatorImpl()));
+//
+//		try {
+//			ctx.registerService(BIPEngine.class.getName(), engine, null);
+//		} catch (Exception ex) {
+//			System.out.println(ex);
+//			throw ex;
+//		}
+//
+//		engine.start();
+//		System.out.println("Bundle providing BIP Engine for Simple usecase is started.");
+		
 	}
 
 	public void stop(BundleContext ctx) {
-		engine.stop();
-		System.out.println("Bundle providing BIP Engine for Simple usecase is stopped.");
+//		engine.stop();
+//		System.out.println("Bundle providing BIP Engine for Simple usecase is stopped.");
 
 	}
 
