8272a750179e79c6595eb1a17a21af21e8e8dac8
Radoslaw Szymanek <radoslaw.szymanek@crossing-tech.com>
2014-06-04 14:09:37 +0200

    Glue xml provided as a fragment, adding BIP domains

--- bip.usecase.cf/bipadmin/src/main/java/io/xtech/bip/admin/commands/ExecuteCommand.java
+++ bip.usecase.cf/bipadmin/src/main/java/io/xtech/bip/admin/commands/ExecuteCommand.java
@@ -8,7 +8,9 @@ package io.xtech.bip.admin.commands;
 
 import java.io.InputStream;
 import java.net.URL;
+import java.util.Enumeration;
 
+import org.apache.felix.gogo.commands.Argument;
 import org.apache.felix.gogo.commands.Command;
 import org.apache.karaf.shell.console.OsgiCommandSupport;
 import org.bip.api.BIPEngine;
@@ -19,27 +21,34 @@ import org.osgi.framework.ServiceReference;
 @Command(scope = "bip", name = "execute", description = "Starts BIP Engine control of the registered components.")
 public class ExecuteCommand extends OsgiCommandSupport {
 
+	@Argument(index = 0, name = "domain", description = "the domainName of the BIP domain", required = true, multiValued = false)
+    String domainName = null;
+	
 	@Override
 	protected Object doExecute() throws Exception {
 
-		String bipGlueFilename = "META-INF/BIP/bipGlueWithData.xml";
-
-		ServiceReference ref = getBundleContext().getServiceReference(BIPEngine.class.getName());
+		String filter = "(domain=" + domainName + ")";
+		
+		ServiceReference[] ref = getBundleContext().getServiceReferences(BIPEngine.class.getName(), filter);
 
 		if (ref != null) {
 
+			ServiceReference firstServiceReference = ref[0];
+			
 			try {
 
-				BIPEngine bipEngine = (BIPEngine) getBundleContext().getService(ref);
+				BIPEngine bipEngine = (BIPEngine) getBundleContext().getService(firstServiceReference);
 
 				if (bipEngine != null) {
 
 					try {
 
-						URL configURL = getBundleContext().getBundle().getResource(bipGlueFilename);
+						String filename = "META-INF/BIP/" + domainName + ".xml";
+
+						URL configURL = getBundleContext().getBundle().getResource(filename);
 
 						if (configURL == null) {
-							System.out.println("I can not find " + bipGlueFilename + " file. ");
+							System.out.println("I can not find glue file " + filename + " for the domain " + domainName);
 							return null;
 						}
 
@@ -50,7 +59,7 @@ public class ExecuteCommand extends OsgiCommandSupport {
 						bipEngine.specifyGlue(bipGlue);
 
 					} catch (Exception ex) {
-						System.out.println("Exception " + ex + "has occurred while trying to handle bip-glue.xml");
+						System.out.println("Exception " + ex + "has occurred while trying to handle bipglue" );
 						System.out.println("Ignoring start command.");
 						return null;
 					}
@@ -58,15 +67,15 @@ public class ExecuteCommand extends OsgiCommandSupport {
 					bipEngine.execute();
 
 				} else {
-					System.out.println("No BIPEngine service available. I do not know what BIP engine to start.");
+					System.out.println("No BIPEngine service for domain " + domainName + " available. I do not know what BIP engine to start.");
 				}
 
 			} finally {
-				getBundleContext().ungetService(ref);
+				getBundleContext().ungetService(firstServiceReference);
 			}
 
 		} else {
-			System.out.println("No BIPEngine service available. I do not know what BIP engine to start.");
+			System.out.println("No BIPEngine service for domain " + domainName + " available. I do not know what BIP engine to start.");
 		}
 
 		return null;
