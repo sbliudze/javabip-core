e8194a5f946657f0b5f04b86f76228fe99341d22
Anastasia Mavridou <anastasia.mavridou@epfl.ch>
2014-12-05 16:32:12 +0100

    FSM for command buffer

--- org.bip.core/org.bip.spec.examples/src/main/java/org/bip/spec/pubsub/CommandBuffer.java
+++ org.bip.core/org.bip.spec.examples/src/main/java/org/bip/spec/pubsub/CommandBuffer.java
@@ -2,6 +2,16 @@ package org.bip.spec.pubsub;
 
 import java.util.LinkedList;
 
+import org.bip.annotations.ComponentType;
+import org.bip.annotations.Guard;
+import org.bip.annotations.Port;
+import org.bip.annotations.Ports;
+import org.bip.annotations.Transition;
+import org.bip.api.PortType;
+
+@Ports({ @Port(name = "putCommand", type = PortType.enforceable),
+		@Port(name = "getCommand", type = PortType.enforceable) })
+@ComponentType(initial = "0", name = "org.bip.spec.Buffer")
 public class CommandBuffer {
 	
 	private LinkedList<Command> commandList;
@@ -13,20 +23,24 @@ public class CommandBuffer {
 		this.commandList = new LinkedList<Command>();
 	}
 
+	@Transition(name = "getCommand", source = "0", target = "0", guard = "isBufferNotEmpty")
 	public Command getCommand(){
 		return commandList.remove();
 	}
-	
-	public boolean existsCommand() {
+
+	@Guard(name = "isBufferNotEmpty")
+	public boolean isBufferEmpty() {
 		return !commandList.isEmpty();
 	}
 	
+	@Transition(name = "putCommand", source = "0", target = "0", guard = "isBufferNotFull")
 	public void putCommand(Command command){
 		commandList.add(command);
 	}
 	
+	@Guard(name = "isBufferNotFull")
 	public boolean isBufferFull() {
-		return commandList.size() == bufferSize;
+		return commandList.size() != bufferSize;
 	}
 	
 }
