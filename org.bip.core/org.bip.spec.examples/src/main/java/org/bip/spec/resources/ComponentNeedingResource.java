c09d63a97b60006de95c670a7473434fdbd8307a
Alina Zolotukhina <alina.zolotukhina@epfl.ch>
2015-09-04 15:17:55 +0200

    resource annotations parsing

--- org.bip.core/org.bip.spec.examples/src/main/java/org/bip/spec/resources/ComponentNeedingResource.java
+++ org.bip.core/org.bip.spec.examples/src/main/java/org/bip/spec/resources/ComponentNeedingResource.java
@@ -1,9 +1,9 @@
 package org.bip.spec.resources;
 
 import org.bip.annotations.ComponentType;
-import org.bip.annotations.Data;
 import org.bip.annotations.Port;
 import org.bip.annotations.Ports;
+import org.bip.annotations.ResourceUtility;
 import org.bip.annotations.ResourcesRequired;
 import org.bip.annotations.Transition;
 import org.bip.annotations.ResourceRequired;
@@ -15,8 +15,9 @@ import org.bip.api.ResourceType;
 public class ComponentNeedingResource {
 
 	@Transition(name = "a", source = "0", target = "0", guard = "")
-	@ResourcesRequired({ @ResourceRequired(label = "processor1", type = ResourceType.processor, utility = "pFunc"),
-			@ResourceRequired(label = "memory1", type = ResourceType.memory, utility = "mFunc") })
+	@ResourcesRequired({ @ResourceRequired(label = "p1", type = ResourceType.processor, utility = "pFunc"),
+			@ResourceRequired(label = "m1", type = ResourceType.memory, utility = "mFunc") })
+	@ResourceUtility(utility="p1=1 & m1=128")
 	public void aTransition() {
 		System.out.print("Doing transition requiring resources");
 	}
