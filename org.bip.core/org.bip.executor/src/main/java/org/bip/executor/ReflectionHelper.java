e6bb97341480f95c2f3b7a2ab5b7822f7b7fc0d6
Alina Zolotukhina <alina.zolotukhina@epfl.ch>
2015-10-28 14:56:37 +0100

    resource annotation can be now treated as parameter annotation

--- org.bip.core/org.bip.executor/src/main/java/org/bip/executor/ReflectionHelper.java
+++ org.bip.core/org.bip.executor/src/main/java/org/bip/executor/ReflectionHelper.java
@@ -13,8 +13,10 @@ import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.bip.annotations.ResourceRequired;
 import org.bip.api.Data;
 import org.bip.api.DataOut.AccessType;
+import org.bip.api.ResourceType;
 import org.bip.exceptions.BIPException;
 
 class ReflectionHelper {
@@ -38,7 +40,7 @@ class ReflectionHelper {
 		if (paramTypes == null || paramTypes.length == 0) {
 			return new ArrayList<Data<?>>();
 		}
-		
+		int resourceParamCount = 0;
 		Annotation[][] paramsAnnotations = method.getParameterAnnotations();
 		for (int i = 0; i < paramsAnnotations.length; i++) {
 			for (Annotation annotation : paramsAnnotations[i]) {
@@ -48,15 +50,48 @@ class ReflectionHelper {
 					dataIn.add(data);
 					break;
 				}
+				else if (annotation instanceof org.bip.annotations.ResourceRequired) {
+					resourceParamCount++;
+				}
 			}
 			// TODO, TEST, write a test that will deal with the case when an annotation is missing. 
 			// Maybe create a default name as specified in Data todo.
-			if (dataIn.size() < i + 1) {
+			// TODO this verification is no longer correct, since some params can be resources
+			if (dataIn.size() + resourceParamCount < i + 1) {
+				//TODO if an annotation is missing, we no longer know whether it is a data or a resource
 				// Data annotation is missing for i-th parameter, thus a default name is taken. 
 				dataIn.add(createData(method.getName() + SEPARATOR + i, paramTypes[i]));
 			}
 		}
 		return dataIn;
+	}
+
+	public static List<ResourceReqImpl> parseResourceAnnotations(Method method) {
+		// deal with method parameters: there might be a dataIn
+		ArrayList<ResourceReqImpl> resourcesRequired = new ArrayList<ResourceReqImpl>();
+		Class<?> paramTypes[] = method.getParameterTypes();
+		if (paramTypes == null || paramTypes.length == 0) {
+			return new ArrayList<ResourceReqImpl>();
+		}
+		
+		Annotation[][] paramsAnnotations = method.getParameterAnnotations();
+		for (int i = 0; i < paramsAnnotations.length; i++) {
+			for (Annotation annotation : paramsAnnotations[i]) {
+				if (annotation instanceof org.bip.annotations.ResourceRequired) {
+					ResourceRequired resourceAnnotation = (ResourceRequired) annotation;
+					//do we need param types here? I guess we do....
+					ResourceReqImpl resourceReq = createResourceRequest(resourceAnnotation.label(), resourceAnnotation.type(), resourceAnnotation.utility(), paramTypes[i]);
+					resourcesRequired.add(resourceReq);
+					break;
+				}
+			}
+			// TODO, verification that there are no not-annotated resources
+		}
+		return resourcesRequired;
+	}
+	
+	private static ResourceReqImpl createResourceRequest(String label, ResourceType type, String utility, Class<?> class1) {
+		return new ResourceReqImpl(label, type, utility);
 	}
 
 	public static <T> Data<T> createData(String dataName, Class<T> type) {
