76191cb348e967dd0e2417dfba148e1574647108
Alina Zolotukhina <alina.zolotukhina@epfl.ch>
2016-04-15 20:25:41 +0200

    some komments about m and D

--- org.bip.core/org.bip.spec.examples/src/main/java/org/bip/spec/resources/KalrayMemory.java
+++ org.bip.core/org.bip.spec.examples/src/main/java/org/bip/spec/resources/KalrayMemory.java
@@ -8,6 +8,7 @@ import org.bip.annotations.Ports;
 import org.bip.annotations.Transition;
 import org.bip.api.PortType;
 import org.bip.api.ResourceProxy;
+import org.bip.resources.ResourceManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -23,10 +24,17 @@ public class KalrayMemory implements ResourceProxy {
 	private int counter;
 	private String id;
 
+	private ResourceManager rManager;
+
 	public KalrayMemory(String id) {
 		this.id = id;
 	}
 
+	public void setResourceManager(ResourceManager manager) {
+		this.rManager = manager;
+	}
+	
+	
 	@Override
 	public String resourceID() {
 		return id;
@@ -39,6 +47,11 @@ public class KalrayMemory implements ResourceProxy {
 		this.counter = readCount;
 		System.err.println("Data " + storedDataName + " created in memory "
 				+ id);
+		//TODO once data is created, inform my manager that I am not available for usage other that for the data
+		// Q: how to understand the type of usage? how can I know, that m is asked because of D?
+		// doubling the data? m_x and md_x?
+		//or dynamically change the links? since we know there is a transition between D and m_x, we know there cannot be a transition between m and m_x ->
+		// not true if we allocate parts of resources.
 	}
 
 	@Transition(name = "read", source = "1", target = "1", guard = "idOK")
@@ -52,6 +65,7 @@ public class KalrayMemory implements ResourceProxy {
 	@Transition(name = "deleteData", source = "1", target = "0", guard = "noUse")
 	public void deleteData() {
 		this.data = "";
+		// TODO once data is deleted, inform my manager I am available again for any usage
 	}
 
 	@Guard(name = "noUse")
