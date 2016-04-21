6cb11d07a7269ecdd61027cc5868ac3588ca4265
Alina <alina.zolotukhina@epfl.ch>
2016-04-21 21:26:02 +0200

    Kalray with data: some comments and asserts

--- org.bip.core/org.bip.spec.examples/src/main/java/org/bip/spec/resources/KalrayMemory.java
+++ org.bip.core/org.bip.spec.examples/src/main/java/org/bip/spec/resources/KalrayMemory.java
@@ -6,17 +6,16 @@ import org.bip.annotations.Guard;
 import org.bip.annotations.Port;
 import org.bip.annotations.Ports;
 import org.bip.annotations.Transition;
+import org.bip.api.DataOut.AccessType;
 import org.bip.api.PortType;
 import org.bip.api.ResourceProxy;
-import org.bip.api.DataOut.AccessType;
-import org.bip.resources.ResourceManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @Ports({ @Port(name = "create", type = PortType.enforceable),
 		@Port(name = "read", type = PortType.enforceable),
 		@Port(name = "deleteData", type = PortType.enforceable)})
-@ComponentType(initial = "0", name = "org.bip.spec.resources.KalrayMemory")
+@ComponentType(initial = "free", name = "org.bip.spec.resources.KalrayMemory")
 public class KalrayMemory implements ResourceProxy {
 
 	Logger logger = LoggerFactory.getLogger(KalrayMemory.class);
@@ -26,21 +25,14 @@ public class KalrayMemory implements ResourceProxy {
 	private String id;
 
 	private MemoryManager rManager;
-	private DataMemoryManager dManager;
 
 	public KalrayMemory(String id) {
 		this.id = id;
 	}
-
-	public KalrayMemory(String id, MemoryManager manager) {
-		this.id = id;
-		this.rManager = manager;
-	}
 	
-	public KalrayMemory(String id, MemoryManager manager, DataMemoryManager dManager) {
-		this.id = id;
+	public KalrayMemory(MemoryManager manager) {
+		this.id = manager.name();
 		this.rManager = manager;
-		this.dManager = dManager;
 	}
 	
 	@Override
@@ -48,7 +40,7 @@ public class KalrayMemory implements ResourceProxy {
 		return id;
 	}
 
-	@Transition(name = "create", source = "0", target = "1", guard = "idOK")
+	@Transition(name = "create", source = "free", target = "hasData", guard = "idOK")
 	public void createData(@Data(name = "data") String storedDataName,
 			@Data(name = "count") int readCount) {
 		this.data = storedDataName;
@@ -56,32 +48,26 @@ public class KalrayMemory implements ResourceProxy {
 		System.err.println("Data " + storedDataName + " created in memory "
 				+ id);
 		rManager.notifyCreation(storedDataName, readCount);
-		// Q: how to understand the type of usage? how can I know, that m is asked because of D?
-		// doubling the data? m_x and md_x?
-		//or dynamically change the links? since we know there is a transition between D and m_x, we know there cannot be a transition between m and m_x ->
-		// not true if we allocate parts of resources.
 	}
 
-	@Transition(name = "read", source = "1", target = "1", guard = "dataIdOK")
+	@Transition(name = "read", source = "hasData", target = "hasData", guard = "dataIdOK")
 	public void readData() {
 		counter--;
 	}
 
-	@Transition(name = "deleteData", source = "1", target = "0", guard = "noUse")
+	@Transition(name = "deleteData", source = "hasData", target = "free", guard = "dataNoLongerNeeded")
 	public void deleteData() {
 		this.data = "";
 		rManager.notifyDeletion(data);
 	}
 
-	@Guard(name = "noUse")
+	@Guard(name = "dataNoLongerNeeded")
 	public boolean dataIsNotUsed() {
-		//System.err.println("noUse in " + this.id + " for data " + this.data + " " + (counter == 0));
 		return counter == 0;
 	}
 
 	@Guard(name = "idOK")
 	public boolean interactionAllowed(@Data(name = "id") String givenId) {
-		//System.out.println("In memory " + this.id + " checking for allowance " + givenId);
 		return this.id == givenId || ("d" + this.id == givenId );
 	}
 	
--- org.bip.core/org.bip.spec.examples/src/main/java/org/bip/spec/resources/MemoryManager.java
+++ org.bip.core/org.bip.spec.examples/src/main/java/org/bip/spec/resources/MemoryManager.java
@@ -5,8 +5,10 @@ import org.bip.resources.BoundedResourceManager;
 public class MemoryManager extends BoundedResourceManager {
 
 	private boolean hasData;
-	private String dataName;
 	private DataMemoryManager dManager;
+	
+	private int dataCount;
+	private int dataDeletionCount;
 	
 	public MemoryManager(String name, int capacity) {
 		super(name, capacity);
@@ -18,21 +20,33 @@ public class MemoryManager extends BoundedResourceManager {
 	
 	public void notifyCreation(String storedDataName, int readCount) {
 		this.hasData = true;
-		this.dataName = storedDataName;
 		dManager.create(storedDataName);
+		dataCount++;
 	}
 
 	public void notifyDeletion(String data) {
 		this.hasData = false;
 		dManager.delete();
+		dataDeletionCount++;
 	}
 	
 	@Override
 	public String cost() {
+		// in case when memory stores data, its cost changes:
+		// the part where memory is stored is not available
+		// (in our case it is the whole memory)
 		if (hasData) {
 			return (resourceName + "=0");
 		}
 		return super.cost();
+	}
+	
+	public int dataCreationCount() {
+		return dataCount;
+	}
+	
+	public int dataDeletionCount() {
+		return dataDeletionCount;
 	}
 
 }
