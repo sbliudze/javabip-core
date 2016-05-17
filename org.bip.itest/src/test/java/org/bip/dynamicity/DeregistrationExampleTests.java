28eb64aaeb31e25307a0890c6fc70a95967f3196
Valentin Rutz <valentin.rutz@epfl.ch>
2016-05-17 10:45:01 +0200

    new Example tests for deregistration

--- org.bip.itest/src/test/java/org/bip/dynamicity/DeregistrationExampleTests.java
+++ org.bip.itest/src/test/java/org/bip/dynamicity/DeregistrationExampleTests.java
@@ -50,7 +50,7 @@ public class DeregistrationExampleTests {
 	public void teardown() {
 		killEngine(engineFactory, engine);
 	}
-	
+
 	@Test
 	public void testExampleDeregistration() {
 		ExampleE e0 = new ExampleE();
@@ -85,5 +85,73 @@ public class DeregistrationExampleTests {
 		engine.deregister(b0);
 
 		sleep(2);
+	}
+
+	@Test
+	public void testExampleDeregistrationOfNecessaryComponent() {
+		ExampleA a0 = new ExampleA(), a1 = new ExampleA(), a2 = new ExampleA();
+		ExampleB b0 = new ExampleB(), b1 = new ExampleB();
+		ExampleC c0 = new ExampleC();
+		
+		engine.register(b0, "b0", true);
+		engine.register(a0, "a0", true);
+		engine.register(a2, "a2", true);
+		engine.register(c0, "c0", true);
+		engine.register(b1, "b1", true);
+		engine.register(a1, "a1", true);
+
+		sleep(4);
+
+		engine.deregister(a0);
+
+		sleep(2);
+
+		engine.deregister(b0);
+
+		sleep(2);
+		
+		engine.deregister(c0);
+		
+		sleep(1);
+	}
+	
+	@Test
+	public void testExampleDeregistrationAndRegistrationStopsAndRestartsEngine() {
+		ExampleA a0 = new ExampleA(), a1 = new ExampleA(), a2 = new ExampleA(), a3 = new ExampleA();
+		ExampleB b0 = new ExampleB(), b1 = new ExampleB(), b2 = new ExampleB();
+		ExampleC c0 = new ExampleC(), c1 = new ExampleC();
+		
+		engine.register(b0, "b0", true);
+		engine.register(a0, "a0", true);
+		engine.register(a2, "a2", true);
+		engine.register(c0, "c0", true);
+		engine.register(b1, "b1", true);
+		engine.register(a1, "a1", true);
+
+		sleep(4);
+
+		engine.deregister(a0);
+
+		sleep(2);
+
+		engine.deregister(b0);
+
+		sleep(2);
+		
+		engine.deregister(c0);
+		
+		sleep(1);
+		
+		engine.register(a3, "a3", true);
+		
+		sleep(1);
+		
+		engine.register(c1, "c1", true);
+		
+		sleep(2);
+		
+		engine.register(b2, "b2", true);
+		
+		sleep(3);
 	}
 }
