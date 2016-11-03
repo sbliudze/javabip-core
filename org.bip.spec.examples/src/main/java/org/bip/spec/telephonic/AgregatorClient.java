5baa96498b525b321cf07e68c37b701484f6b9a3
Alina Zolotukhina <alina.zolotukhina@epfl.ch>
2016-11-03 14:33:29 +0100

    telephonic usecase removed to a different repository

--- org.bip.spec.examples/src/main/java/org/bip/spec/telephonic/AgregatorClient.java
+++
@@ -1,8 +0,0 @@
-package org.bip.spec.telephonic;
-
-import org.bip.api.BIPActor;
-
-public interface AgregatorClient extends ClientCaller {
-	public void setExecutorRefs(BIPActor caller, BIPActor callee,
-			BIPActor voice, BIPActor disc, BIPActor client, BIPActor checker);
-}
--- org.bip.spec.examples/src/main/java/org/bip/spec/telephonic/ClientCaller.java
+++
@@ -1,8 +0,0 @@
-package org.bip.spec.telephonic;
-
-import org.bip.api.BIPActor;
-
-public interface ClientCaller {
-	public void setClientRefs(BIPActor client, int id);
-	public void setSyncRefs(BIPActor syncActor);
-}
