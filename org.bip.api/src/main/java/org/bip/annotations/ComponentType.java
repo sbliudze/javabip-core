115084f5a6b3c26953edc7642bd6d873ed7873f5
Alina Zolotukhina <alina.zolotukhina@epfl.ch>
2016-11-18 17:05:29 +0100

    pom files for bip core cleaned

--- org.bip.api/src/main/java/org/bip/annotations/ComponentType.java
+++ org.bip.api/src/main/java/org/bip/annotations/ComponentType.java
@@ -1,6 +1,18 @@
 /*
- * Copyright (c) 2012 Crossing-Tech TM Switzerland. All right reserved.
- * Copyright (c) 2012, RiSD Laboratory, EPFL, Switzerland.
+ * Copyright 2012-2016 Ecole Polytechnique Federal de Lausanne (EPFL), Switzerland
+ * Copyright 2012-2016 Crossing-Tech TM Switzerland
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  * Author: Simon Bliudze, Alina Zolotukhina, Anastasia Mavridou, and Radoslaw Szymanek
  * Date: 10/15/12
